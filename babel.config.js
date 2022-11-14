83954fb03e44afb010b2e43ea5391bef527de009
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-11-14 15:25:14 +0100

    Fixed Drawer

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+    plugins: [
+      'react-native-reanimated/plugin', // This line.
+    ],
     presets: ['babel-preset-expo'],
   };
 };
