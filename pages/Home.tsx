0a29e7646b6b1c4af8b7a39b30df9d42adf7cd52
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-11-28 13:15:27 +0100

    Begin Homepage

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,10 +1,17 @@
+import * as React from 'react';
+import {useState} from 'react';
+import { Text, View, TextInput, StyleSheet, StyleProp, ViewStyle,Button } from 'react-native';
+import Constants from 'expo-constants';
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Characters from './Characters';
+import Comics from './Comics';
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { FontAwesome5  } from '@expo/vector-icons'; 
+
 const Home = () =>
-{
-    return (
-        <>
-        </>
-    )
-}
-
-
+<Text>
+Welcome at the Marvel page. Here you can look for characters and commics from Marvel.
+</Text>
 export default Home;
