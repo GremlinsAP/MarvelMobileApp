3b4bc25af07fe461b2a0fa77feae132f7622f481
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-12-19 15:21:34 +0100

    comicDetail

--- pages/ComicDetail.tsx
+++ pages/ComicDetail.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
 import Layout from "../components/Layout";
 import { Comic } from "../util/interfaces/Comic";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
 import { RouteProp, useRoute } from "@react-navigation/native";
+import NamedSection from "../components/NamedSection";
+import { DetailListPrint } from "../util/RenderUtil";
+
 
 type ComicDetailProps = {
     comic: Comic;
@@ -13,37 +16,50 @@ const ComicDetail = () => {
         params: { comic },
     }: RouteProp<Record<string, ComicDetailProps>> = useRoute();
     return <Layout>
-        <View>
-            <Text>Name: {comic.title}</Text>
-            <Text>Series: {comic.series.name}</Text>
-            <Text>Description: {comic.description}</Text>
-        </View>
-        <View>
+        <ScrollView>
+        <NamedSection title="Main Info">
+            {DetailListPrint({
+            Name: comic.title,
+            Series: comic.series.name,
+            Description: comic.description.length > 60
+            ? comic.description.substring(0, 500).concat("...")
+            : comic.description ||
+              "This comic does not have a description."
+            })}
+        </NamedSection>
+        <NamedSection title="Thumbnail">
             <Image
                 style={styles.image}
                 source={{
-                    uri: `${comic.thumbnail.path}.${comic.thumbnail.extension}`,
+                    uri: `${comic.thumbnail.path.endsWith("image_not_available")? "https://i.redd.it/7z6qt753qe031" : comic.thumbnail.path}.${comic.thumbnail.extension}`,
                 }}
             />
-        </View>
-        <View>
-        {comic.stories.items.map((story) => {
+        </NamedSection>
+        <NamedSection title="Characters">
+        {comic.characters.items.map((character) => {
         return (
-          <View>
-            <Text>{story.name}</Text>
-          </View>
+            <Text>{character.name.endsWith("")? "this comic has no characters available": character.name}</Text>
         );
       })}
-        </View>
+        </NamedSection>
+        <NamedSection title="Creators">
+        {comic.creators.items.map((creator) => {
+        return (
+            <Text>{creator.name}</Text>
+        );
+      })}
+        </NamedSection>
+        </ScrollView>
     </Layout>
+    
 }
 
 export default ComicDetail;
 
 const styles = StyleSheet.create({
     image: {
-        width: 150,
-        height: 150,
+        width:  300,
+        height: 450,
         alignSelf: "center"
     },
 });
