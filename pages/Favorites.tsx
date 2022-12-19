8d305074647f684a02043543f3e191d901bc783c
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-19 18:32:13 +0100

    Base Card

--- pages/Favorites.tsx
+++ pages/Favorites.tsx
@@ -1,8 +1,11 @@
+import { ScrollView } from "react-native-gesture-handler";
 import Layout from "../components/Layout";
 
 const Favorites = () => {
   return <Layout>
-    
+    <ScrollView>
+      
+    </ScrollView>
   </Layout>;
 };
 
