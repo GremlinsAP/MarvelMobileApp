9f3e3cb3a44914f7f9dd70466c961ad8659bbace
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-22 14:28:34 +0100

    Favorite

--- pages/Favorites.tsx
+++ pages/Favorites.tsx
@@ -1,12 +1,36 @@
 import { ScrollView } from "react-native-gesture-handler";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
+import { Favorites } from "../util/interfaces/Favorites";
+import { getData } from "../util/LocalStorage";
+import NamedSection from "../components/NamedSection";
+import FavoriteEntry from "../components/FavoriteEntry";
 
-const Favorites = () => {
+const FavoritesPage = () => {
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [favorites, setFavorites] = useState<Favorites>();
+
+  useEffect(() => {
+    const fetch = async () => {
+      setFavorites(await getData());
+      setLoading(false);
+    }
+
+    fetch();
+  }, []);
+
   return <Layout>
     <ScrollView>
-      
+      {!loading && <NamedSection title="Characters">
+        {favorites?.favoriteCharacters.map((c, i) => <FavoriteEntry key={i} type="character" id={c.id} />)}
+      </NamedSection>}
+
+      {!loading && <NamedSection title="Comics">
+        {favorites?.favoriteComics.map((c, i) => <FavoriteEntry key={i} type="comic" id={c.id} />)}
+      </NamedSection>}
     </ScrollView>
   </Layout>;
 };
 
-export default Favorites;
+export default FavoritesPage;
